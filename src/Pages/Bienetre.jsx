16228f08eb90a345c0694e299e6d48de9bba1adb
KabsiMontassar <114872623+KabsiMontassar@users.noreply.github.com>
2024-08-22 19:08:44 +0100

    push

---
+++ src/Pages/Bienetre.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bienetre = () => {
+  return (
+    <div>Bienetre</div>
+  )
+}
+
+export default Bienetre
---
+++ src/Pages/Rendezvous.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Rendezvous = () => {
+  return (
+    <div>Rendezvous</div>
+  )
+}
+
+export default Rendezvous
