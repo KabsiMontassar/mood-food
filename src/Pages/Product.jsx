74cad15dfb74cf5ae8783c6550f49e568dd2fb8a
AmelJaballah <151528543+AmelJaballah@users.noreply.github.com>
2024-09-11 23:08:39 +0100

    Update Product.jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -27,6 +27,9 @@ const customTheme = extendTheme({
     green: {
       500: '#32a852',
     },
+    orange: {
+      500: '#f06c00',
+    },
   },
 });
 
@@ -97,22 +100,22 @@ const initialProducts = [
   },
 ];
 
-// HeaderSection Component
+// HeaderSection Component with Carousel Dots
 const HeaderSection = ({ onCategorySelect, selectedCategory }) => {
   const categories = [
     {
       title: 'Produit Diététique',
-      description: 'Description for Produit Diététique category.',
+      description: 'Découvrez nos produits diététiques pour une alimentation équilibrée, savoureuse et adaptée à vos objectifs de santé.',
       image: '/src/assets/images/diététiques.jpeg',
     },
     {
       title: 'Complement alimentaire',
-      description: 'Description for Complement alimentaire category.',
+      description: 'Boostez votre santé avec nos compléments alimentaires de qualité, conçus pour répondre à vos besoins spécifiques.',
       image: '/src/assets/images/complement.jpg',
     },
     {
       title: 'Equipement sportif',
-      description: 'Equipment sportif for strength and endurance training.',
+      description: 'Équipez-vous avec notre sélection d’équipements et accessoires sportifs, parfaits pour atteindre vos performances optimales.',
       image: '/src/assets/images/sportif.webp',
     },
   ];
@@ -128,7 +131,16 @@ const HeaderSection = ({ onCategorySelect, selectedCategory }) => {
     }
   }, [selectedCategory]);
 
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % categories.length);
+  };
+
   const currentCategoryData = categories[currentIndex];
+
+  useEffect(() => {
+    const interval = setInterval(handleNext, 3000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <Box mb={6} position="relative" width="100%">
@@ -136,9 +148,9 @@ const HeaderSection = ({ onCategorySelect, selectedCategory }) => {
         <Image
           src={currentCategoryData.image}
           alt={currentCategoryData.title}
-          width="100%"  // Full width
-          height="400px" // Fixed height
-          objectFit="cover" // Ensures image covers the container
+          width="100%"
+          height="400px"
+          objectFit="cover"
           borderRadius="md"
         />
 
@@ -149,33 +161,61 @@ const HeaderSection = ({ onCategorySelect, selectedCategory }) => {
           left="0"
           width="100%"
           height="100%"
-          bg="rgba(0, 0, 0, 0.5)" // Semi-transparent background
+          bg="rgba(0, 0, 0, 0.5)"
           display="flex"
           flexDirection="column"
           justifyContent="center"
           alignItems="center"
           borderRadius="md"
-          boxShadow="0 4px 6px rgba(0, 0, 0, 0.3)" // Shadow effect
+          boxShadow="0 4px 6px rgba(0, 0, 0, 0.3)"
         >
           <Box textAlign="center" color="white" p={4}>
-            <Heading size="lg" mb={2}>{currentCategoryData.title}</Heading>
+            <Heading size="lg" mb={2}>
+              {currentCategoryData.title}
+            </Heading>
             <Text>{currentCategoryData.description}</Text>
           </Box>
         </Box>
+
+        {/* Dots for carousel */}
+        <HStack spacing={4} position="absolute" bottom={4} left="50%" transform="translateX(-50%)">
+          {categories.map((_, idx) => (
+            <Box
+              key={idx}
+              width={2}
+              height={2}
+              bg={idx === currentIndex ? 'orange.500' : 'white'}
+              borderRadius="50%"
+            />
+          ))}
+        </HStack>
       </Box>
 
       {/* Category buttons */}
       <HStack spacing={4} justifyContent="center" mt={4}>
-        {categories.map((cat) => (
-          <Button
-            key={cat.title}
-            colorScheme="green"
-            onClick={() => onCategorySelect(cat.title)}
-            variant={selectedCategory === cat.title ? 'solid' : 'outline'}
-          >
-            {cat.title}
-          </Button>
-        ))}
+        {categories.map((cat) => {
+          const isActive = selectedCategory === cat.title;
+          return (
+            <Button
+              key={cat.title}
+              onClick={() => onCategorySelect(cat.title)}
+              variant={isActive ? 'solid' : 'outline'}
+              bgImage={`url(${cat.image})`}
+              bgSize="cover"
+              bgPosition="center"
+              color={isActive ? 'white' : 'black'}
+              px={4}
+              py={2}
+              minWidth="400px"
+              height="50px"
+              border="none"
+              boxShadow={isActive ? 'none' : '0px 4px 6px rgba(0, 0, 0, 0.3)'} // Gray shadow for inactive buttons
+              transition="box-shadow 0.3s ease-in-out" // Smooth transition for shadow
+            >
+              {cat.title}
+            </Button>
+          );
+        })}
       </HStack>
     </Box>
   );
@@ -184,27 +224,35 @@ const HeaderSection = ({ onCategorySelect, selectedCategory }) => {
 // ProductDetails Component
 const ProductDetails = ({ product, onBackClick, onAddToCart }) => (
   <Box p={6}>
-    <Button onClick={onBackClick} colorScheme="green" mb={4}>Back to Products</Button>
-    <Grid templateColumns="1fr 2fr" gap={6}>
-      <Image
-        src={product.image}
-        alt={product.name}
-        boxSize="300px"
-        objectFit="cover"
-        borderRadius="md"
-      />
+    <Button onClick={onBackClick} colorScheme="orange" mb={4}>Back to Products</Button>
+    <Grid templateColumns={{ base: '1fr', md: '1fr 2fr' }} gap={6}>
+      {/* Left side - Image */}
       <Box>
-        <Heading>{product.name}</Heading>
+        <Image
+          src={product.image}
+          alt={product.name}
+          boxSize={{ base: '100%', md: '300px' }}
+          objectFit="cover"
+          borderRadius="md"
+        />
+      </Box>
+      {/* Right side - Details */}
+      <Box>
+        <Text fontSize="sm" color="gray.500" mb={2}>{product.category}</Text> {/* Category */}
+        <Heading size="lg" mb={2}>{product.name}</Heading>
+        <Box borderBottom="2px" borderColor="black" mb={4} /> {/* Black break line */}
         <Text fontSize="lg" mb={4}>{product.description}</Text>
-        <Text fontSize="2xl" color="green.500">{product.price} DT</Text>
-        <Button colorScheme="green" mt={4} onClick={() => onAddToCart(product)}>Add to Cart</Button>
+        <Text fontSize="2xl" color="orange.500" mb={4}>{product.price} DT</Text>
+        <Button colorScheme="orange" onClick={() => onAddToCart(product)}>Add to Cart</Button>
       </Box>
     </Grid>
   </Box>
 );
 
-// ProductGrid Component
-const ProductGrid = ({ selectedCategory }) => {
+// ProductGrid Component with Sidebar and 4 products per row
+
+
+const ProductGrid = ({ selectedCategory, onCategorySelect }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [priceSortOrder, setPriceSortOrder] = useState('ascending');
   const [selectedKeywords, setSelectedKeywords] = useState([]);
@@ -224,6 +272,14 @@ const ProductGrid = ({ selectedCategory }) => {
     );
   };
 
+  const handleCategoryClick = (category) => {
+    onCategorySelect(category);
+  };
+
+  const handleShowAllClick = () => {
+    onCategorySelect('');
+  };
+
   const handleInfoClick = (product) => setShowDetails(product);
   const handleBackClick = () => setShowDetails(null);
   const handleAddToCartClick = (product) => {
@@ -237,128 +293,164 @@ const ProductGrid = ({ selectedCategory }) => {
     });
   };
 
-  const filteredProducts = initialProducts.filter((product) => {
-    const matchesSearch =
-      product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      product.description.toLowerCase().includes(searchTerm.toLowerCase());
-    const matchesCategory = selectedCategory ? product.category === selectedCategory : true;
-    const matchesKeywords = selectedKeywords.length === 0 || selectedKeywords.includes(product.category);
+  const filteredProducts = initialProducts
+    .filter((product) =>
+      (!selectedCategory || product.category === selectedCategory) &&
+      (!searchTerm || product.name.toLowerCase().includes(searchTerm.toLowerCase())) &&
+      (selectedKeywords.length === 0 || selectedKeywords.every((keyword) =>
+        product.description.toLowerCase().includes(keyword.toLowerCase())
+      ))
+    )
+    .sort((a, b) =>
+      priceSortOrder === 'ascending' ? a.price - b.price : b.price - a.price
+    );
 
-    return matchesSearch && matchesCategory && matchesKeywords;
-  });
+  const categories = [
+    'Produit Diététique',
+    'Complement alimentaire',
+    'Equipement sportif',
+  ];
 
-  const sortedProducts = filteredProducts.sort((a, b) => {
-    return priceSortOrder === 'ascending' ? a.price - b.price : b.price - a.price;
-  });
+  const keywords = [
+    'high-quality', 'muscle mass', 'full-body workout', 'protein shake', 'comfort', 'lightweight', 'strength training', 'zinc', 'vitamin C',
+  ];
 
-  return showDetails ? (
-    <ProductDetails
-      product={showDetails}
-      onBackClick={handleBackClick}
-      onAddToCart={handleAddToCartClick}
-    />
-  ) : (
-    <Box border="1px solid #ccc" borderRadius="md" p={4}>
-      {/* Filters and Sort */}
-      <Flex mb={4}>
-        <Box p={4} width="250px" borderRight="1px solid #ccc" mr={4}>
-          <Heading size="md" mb={2}>Keywords</Heading>
-          <Stack spacing={2}>
-            {['Produit Diététique', 'Complement alimentaire', 'Equipement sportif', 'Supplements', 'Accessories', 'Apparel'].map((keyword) => (
-              <Checkbox
-                key={keyword}
-                isChecked={selectedKeywords.includes(keyword)}
-                onChange={() => handleKeywordChange(keyword)}
-              >
-                {keyword}
-              </Checkbox>
-            ))}
-          </Stack>
-        </Box>
-
-        <Box flex="1">
-          <HStack mb={4} spacing={4} justify="flex-end">
-            <Input
-              value={searchTerm}
-              onChange={handleSearchChange}
-              placeholder="Search for a product"
-              width="300px"
-              mr={2}
-            />
-            <Select value={priceSortOrder} onChange={handlePriceSortOrderChange} width="200px">
-              <option value="ascending">Price: Low to High</option>
-              <option value="descending">Price: High to Low</option>
-            </Select>
-          </HStack>
-
-          {/* Product Grid */}
-          <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={6}>
-            {sortedProducts.map((product) => (
-              <Box
-                key={product.productId}
-                border="1px solid #ccc"
-                borderRadius="md"
-                overflow="hidden"
-                position="relative"
-                onMouseEnter={() => setHoveredProduct(product.productId)}
-                onMouseLeave={() => setHoveredProduct(null)}
-              >
-                <Image
-                  src={product.image}
-                  alt={product.name}
-                  boxSize="300px"
-                  objectFit="cover"
-                />
-                <Box p={4}>
-                  <Heading size="md">{product.name}</Heading>
-                  <Text fontSize="lg">{product.price} DT</Text>
-                </Box>
-                {hoveredProduct === product.productId && (
-                  <Box
-                    position="absolute"
-                    bottom="0"
-                    left="0"
-                    right="0"
-                    p={2}
-                    textAlign="center"
-                    bg="rgba(0, 0, 0, 0.6)"
-                    color="white"
-                    cursor="pointer"
-                    onClick={() => handleInfoClick(product)}
+  return (
+       <Box p={6}>
+      {showDetails ? (
+        <ProductDetails
+          product={showDetails}
+          onBackClick={handleBackClick}
+          onAddToCart={handleAddToCartClick}
+        />
+      ) : (
+        <Flex direction="row" spacing={6}>
+          {/* Sidebar - Category Filter */}
+          <Box flex="1" mr={6} borderWidth="1px" borderRadius="md" borderColor="gray.200" p={4} bg="white">
+            <Heading size="md" mb={4}>Categories</Heading>
+            <Button
+              onClick={handleShowAllClick}
+              variant={selectedCategory === '' ? 'solid' : 'outline'}
+              colorScheme="orange"
+              width="100%"
+              mb={2}
+            >
+              Tout afficher
+            </Button>
+            <Stack spacing={2}>
+              {categories.map((category) => (
+                <Button
+                  key={category}
+                  onClick={() => handleCategoryClick(category)}
+                  variant={selectedCategory === category ? 'solid' : 'outline'}
+                  colorScheme="orange"
+                  width="100%"
+                  size="sm"
+                >
+                  {category}
+                </Button>
+              ))}
+            </Stack>
+            <Box mt={6}>
+              <Heading size="md">Mot-clé</Heading>
+              <Stack spacing={2} mt={2}>
+                {keywords.map((keyword) => (
+                  <Checkbox
+                    key={keyword}
+                    isChecked={selectedKeywords.includes(keyword)}
+                    onChange={() => handleKeywordChange(keyword)}
                   >
-                    Plus Details
+                    {keyword}
+                  </Checkbox>
+                ))}
+              </Stack>
+            </Box>
+          </Box>
+
+          {/* Search Bar and Product Grid */}
+          <Box flex="3">
+            <Box borderWidth="1px" borderRadius="md" borderColor="gray.200" p={4} mb={6}>
+              <HStack spacing={4} mb={6} justifyContent="flex-end">
+                <Input
+                  placeholder="Search Products..."
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                  width="700px"
+                />
+                <Select
+                  value={priceSortOrder}
+                  onChange={handlePriceSortOrderChange}
+                  width="200px"
+                >
+                  <option value="ascending">Price: Low to High</option>
+                  <option value="descending">Price: High to Low</option>
+                </Select>
+              </HStack>
+
+              <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+                {filteredProducts.map((product) => (
+                  <Box
+                    key={product.productId}
+                    borderWidth={1}
+                    borderRadius="md"
+                    overflow="hidden"
+                    p={4}
+                    bg="white"
+                    boxShadow="md"
+                    position="relative"
+                    onMouseEnter={() => setHoveredProduct(product.productId)}
+                    onMouseLeave={() => setHoveredProduct(null)}
+                  >
+                    <Image
+                      src={product.image}
+                      alt={product.name}
+                      width="100%"
+                      height="200px"
+                      objectFit="cover"
+                    />
+                    {hoveredProduct === product.productId && (
+                      <Button
+                        colorScheme="orange"
+                        position="absolute"
+                        bottom="0"
+                        left="0"
+                        width="100%"
+                        borderRadius="none"
+                        onClick={() => handleInfoClick(product)}
+                      >
+                        Details
+                      </Button>
+                    )}
+                    <Box mt={2}>
+                      <Text fontWeight="bold" color="orange.500">{product.category}</Text>
+                      <Text fontWeight="bold">{product.name}</Text>
+                      <Text fontSize="lg" color="orange.500">{product.price} DT</Text>
+                    </Box>
                   </Box>
-                )}
-              </Box>
-            ))}
-          </Grid>
-        </Box>
-      </Flex>
+                ))}
+              </Grid>
+            </Box>
+          </Box>
+        </Flex>
+      )}
     </Box>
   );
 };
-
-// App Component
+// Main App Component
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState('');
 
-  const handleCategorySelect = (category) => {
-    setSelectedCategory(category);
-  };
-
   return (
     <ChakraProvider theme={customTheme}>
-      <Box
-        bg="blackWhite.background"
-        color="blackWhite.text"
-        minHeight="100vh"
-        p={6}
-      >
+      <Box p={6} bg="blackWhite.background" minHeight="100vh" color="blackWhite.text">
         <HeaderSection
-          onCategorySelect={handleCategorySelect}
+          onCategorySelect={setSelectedCategory}
           selectedCategory={selectedCategory}
         />
-        <ProductGrid selectedCategory={selectedCategory} />
+        <ProductGrid
+          selectedCategory={selectedCategory}
+          onCategorySelect={setSelectedCategory}
+        />
       </Box>
     </ChakraProvider>
   );
